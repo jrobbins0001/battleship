bd2faa471ded1ec4bec4cbd57d64bb74d897f0c8
jrobbins0001 <jrobbins0001@mymail.lausd.net>
2017-03-07 12:45:20 -0800

    Create battleship.py

---
+++ battleship.py
@@ -0,0 +1,57 @@
+from random import randint
+class color:
+   PURPLE = '\033[95m'
+   CYAN = '\033[96m'
+   DARKCYAN = '\033[36m'
+   BLUE = '\033[94m'
+   GREEN = '\033[92m'
+   YELLOW = '\033[93m'
+   RED = '\033[91m'
+   BOLD = '\033[1m'
+   UNDERLINE = '\033[4m'
+   END = '\033[0m'
+board = []
+
+for x in range(5):
+    board.append(["O"] * 5)
+
+def print_board(board):
+    for row in board:
+        print " ".join(row)
+
+print "Let's play Battleship!"
+print_board(board)
+
+def random_row(board):
+    return randint(0, len(board) - 1)
+
+def random_col(board):
+    return randint(0, len(board[0]) - 1)
+
+ship_row = random_row(board)
+ship_col = random_col(board)
+
+
+for turn in range(4):
+    X = color.RED+'X'
+    print "Turn", turn+1
+    guess_row = int(raw_input("Guess Row:")) -1
+    guess_col = int(raw_input("Guess Col:")) -1
+    print ""
+
+    if guess_row == ship_row and guess_col == ship_col:
+        print "Congratulations! You sunk my battleship!"
+        break
+    else:
+        if (guess_row < 0 or guess_row > 4) or (guess_col < 0 or guess_col > 4):
+            print "Oops, that's not even in the ocean."
+        elif(board[guess_row][guess_col] == "X"):
+            print "You guessed that one already."
+        else:
+            print color.RED +  "You missed my battleship!"
+            board[guess_row][guess_col] = "%s" % X
+            if turn == 3:
+                print "Game Over"
+                
+   
+    print_board(board)
